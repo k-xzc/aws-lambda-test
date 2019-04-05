f3ae8f04ce6de7bff2ef91139e7bd6683c43d981
kantsuw <kantsuwan@gmail.com>
2019-04-05 22:43:55 +0700

    Update lambda.tf

--- lambda.tf
+++ lambda.tf
@@ -39,3 +39,14 @@ resource "aws_iam_role" "lambda_exec" {
 }
 EOF
 }
+
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.example.arn}"
+  principal     = "apigateway.amazonaws.com"
+
+  # The /*/* portion grants access from any method on any resource
+  # within the API Gateway "REST API".
+  source_arn = "${aws_api_gateway_deployment.example.execution_arn}/*/*"
+}
